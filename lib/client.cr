ea8c8a4b47fdab0acde0cf8370000c03dbe537c2
Greg Donald <gdonald@gmail.com>
2019-02-03 18:15:24 -0600

    join channels

--- lib/client.cr
+++ lib/client.cr
@@ -2,47 +2,60 @@ require "json"
 require "socket"
 
 class Client
-  getter version : String = "crystal-bot v0.0.1"
+  getter version : String = "crystal-irc-bot v0.0.1"
 
   property server : String = ""
-  property port : Int32 = 6667
+  property port : Int32 = 0
   property nick : String = ""
   property user : String = ""
+  property password : String = ""
+  property channels : Array(String) = [] of String
 
   property client : TCPSocket
   property response_count : Int32 = 0
+  property logged_in : Bool = false
+  property version_sent : Bool = false
+  property channels_joined : Bool = false
 
   def initialize
     configure
-    @client = TCPSocket.new(@server, @port)
+    @client = TCPSocket.new(server, port)
     
     while true
       sleep 0.1
-
-      response = client.gets
+      response = get_response
       next unless response
-      puts response
-      @response_count += 1
 
       login
       privmsg(response)
+      join_channels
+      break if logged_in && version_sent && channels_joined
+    end
+
+    while true
+      sleep 0.1
+      response = get_response
+      next unless response
+
       pong(response)
     end
     
-    @client.close
+    client.close
   end
 
   def login
     return unless @response_count == 3
-    @client << "PASS none\r\n"
-    @client << "NICK #{nick}\r\n"
-    @client << "USER #{user} 8 * :#{user}\r\n"
+    client << "PASS #{password}\r\n"
+    client << "NICK #{nick}\r\n"
+    client << "USER #{user} 8 * :#{user}\r\n"
+    @logged_in = true
   end
 
   def version(response)
     return unless response.includes?("VERSION")
     puts "VERSION #{version}"
-    @client << "VERSION #{version}\r\n"
+    client << "VERSION #{version}\r\n"
+    @version_sent = true
   end
 
   def privmsg(response)
@@ -55,7 +68,25 @@ class Client
     parts = response.split(":")
     return unless parts.size == 2
     puts "PONG :#{parts[1]}"
-    @client << "PONG :#{parts[1]}\r\n"
+    client << "PONG :#{parts[1]}\r\n"
+  end
+
+  def get_response
+    response = @client.gets
+    if response
+      puts response
+      @response_count += 1
+    end
+    response
+  end
+
+  def join_channels
+    return unless version_sent || channels_joined
+    channels.each do |channel|
+      client << "JOIN #{channel}\r\n"
+      sleep 0.2
+    end
+    @channels_joined = true
   end
 
   def configure
@@ -64,5 +95,9 @@ class Client
     @port = json["port"].to_s.to_i
     @nick = json["nick"].to_s
     @user = json["user"].to_s
+    @password = json["password"].to_s
+    json["channels"].as_a.each do |channel|
+      @channels.push(channel.as_s)
+    end
   end
 end
